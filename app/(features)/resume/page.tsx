3fc462e3cdf0c76079c0fd5ae12a95c5e6745ee8
Red <shiraphyre@gmail.com>
2024-09-14 17:07:14 -0600

    Button :D
    A singular button

--- app/(features)/resume/page.tsx
+++ app/(features)/resume/page.tsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import styles from './button.module.css'; // Adjust the path as necessary
+
 export default function Resume() {
-  return <div></div>;
+
+  return (
+    <div>
+      <button 
+        id='b1' 
+        className={styles.pageButton} // Apply the button class
+      >
+        <img 
+          src="resumeAdd.jpg" 
+          alt="Page Icon" 
+        />
+      </button>
+    </div>
+  );
 }
